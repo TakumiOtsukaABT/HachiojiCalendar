c377b90d371b5451a5b203b9c4f757888fa1b280
Takumi Otsuka <takumi.otsuka@styler.link>
2021-05-29 17:00:02 +0900

    district 10

--- GarBageCalendar2/ViewController.swift
+++ GarBageCalendar2/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var settingButton: UIButton!
     
     var pickerView = UIPickerView()
-    let list = ["地区1", "地区2","地区3","地区4","地区5","地区6","地区7","地区8","地区9"]
+    let list = ["地区1", "地区2","地区3","地区4","地区5","地区6","地区7","地区8","地区9","地区10"]
     var district = 0
     
     let userDefaults = UserDefaults.standard
